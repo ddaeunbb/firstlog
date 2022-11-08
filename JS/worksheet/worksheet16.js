772539dff6bf37d66275f5c276c6dc4c1ae3328c
ddaeunbb <ddaeunbb@gmail.com>
2022-11-08 17:37:37 +0900

    JS json파일 수정

--- JS/worksheet/worksheet16.js
+++ JS/worksheet/worksheet16.js
@@ -1,4 +1,28 @@
-$.get('./store.json')
+$.get('https://ddaeunbb.github.io/JS/worksheet/store.json')
 .done(function(data){
-  console.log(data)
-});
+  var products = data.products;
+  console.log(products);
+
+
+  products.forEach((a, i)=>{
+    var card = `<div class="card">
+    <div class="card-img">
+      <img src="${products[i].photo}" alt="">
+    </div>
+    <div class="card-text">
+      <h3>${products[i].title}</h3>
+      <h4>${products[i].brand}</h4>
+      <h4>${products[i].price}원</h4>
+    </div>
+    <div class="buybtn">
+      <button class="buy">담기</button>
+    </div>
+    <div style="clear: both;"></div>
+  </div>`
+
+  $('.card-container').append(card);
+  })
+})
+
+
+
