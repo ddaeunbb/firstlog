11bdf5769be5badc643515290e986d55e12ab026
ddaeunbb <ddaeunbb@gmail.com>
2022-11-15 19:52:42 +0900

    15일 2차 공부

--- coding test/search.js
+++ coding test/search.js
@@ -2,7 +2,8 @@ var searchData = [
   {id: 0, title : "#array #object sum(합) 구하기", src : "./day10/day10.html", date: "22.11.10", content: "콜백함수 활용해서 array, object 정렬하기"},
   {id: 1, title : "#reverse 문자열, array 반대정렬하기", src : "./day14/day14-1.html", date: "22.11.14", content: "reverse API"},
   {id: 2, title : "#else if #?, #sort, #..., #repeat", src : "./day14/day14-2.html", date: "22.11.14", content: "array 정렬 함수"},
-  {id: 3, title : "#gcd최대공약수 #for반복문 #Map() #array안 array", src : "./day15/day15.html", date: "22.11.15", content: "array 정렬 함수"}
+  {id: 3, title : "#gcd최대공약수 #for반복문 #new Map <br>#array안 array #new Set", src : "./day15/day15-1.html", date: "22.11.15", content: "array 정렬 함수"},
+  {id: 4, title : "#toString #reduce #Number #fill #Array", src : "./day15/day15-2.html", date: "22.11.15", content: "array 정렬 함수"},
 ]
 
 // 처음에 들어왔을 때, 리스트 생성
