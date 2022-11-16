d244d2bb36f19be91d4c6ff74892c656fcf763c4
ddaeunbb <ddaeunbb@gmail.com>
2022-11-16 16:46:50 +0900

    16일 공부

--- coding test/search.js
+++ coding test/search.js
@@ -2,8 +2,11 @@ var searchData = [
   {id: 0, title : "#array #object sum(합) 구하기", src : "./day10/day10.html", date: "22.11.10", content: "콜백함수 활용해서 array, object 정렬하기"},
   {id: 1, title : "#reverse 문자열, array 반대정렬하기", src : "./day14/day14-1.html", date: "22.11.14", content: "reverse API"},
   {id: 2, title : "#else if #?, #sort, #..., #repeat", src : "./day14/day14-2.html", date: "22.11.14", content: "array 정렬 함수"},
-  {id: 3, title : "#gcd최대공약수 #for반복문 #new Map <br>#array안 array #new Set", src : "./day15/day15-1.html", date: "22.11.15", content: "array 정렬 함수"},
-  {id: 4, title : "#toString #reduce #Number #fill #Array", src : "./day15/day15-2.html", date: "22.11.15", content: "array 정렬 함수"},
+  {id: 3, title : "#gcd최대공약수 #for반복문 #new Map <br>#array안 array #new Set", src : "./day15/day15-1.html", date: "22.11.15", content: ""},
+  {id: 4, title : "#toString #reduce #Number #fill #Array", src : "./day15/day15-2.html", date: "22.11.15", content: ""},
+  {id: 5, title : "#gcd #lcd #유클리드호제법", src : "./day16/day16-1.html", date: "22.11.16", content: "array 정렬 함수"},
+  {id: 6, title : "#문자를 array로 변환 #split #[...] #array를 문자 변수로 변환 #array 숫자열로 변환", src : "./day16/day16-2.html", date: "22.11.16", content: "문자열 <--> array"},
+  {id: 7, title : "#정규식", src : "./day16/day16-3.html", date: "22.11.16", content: "정규식 /[a-z]/"}
 ]
 
 // 처음에 들어왔을 때, 리스트 생성
