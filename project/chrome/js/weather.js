aa6110e2058d15bd7edd0e9382fcab23dcc066eb
ddaeunbb <ddaeunbb@gmail.com>
2023-01-18 15:01:50 +0900

    2023년 1월 18일 깃커밋

---
+++ project/chrome/js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "3c98b8415874832af31ce4ea1b2af530";
+
+function onGeoOk(position) {
+  console.log(position);
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
